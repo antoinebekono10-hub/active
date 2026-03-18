2a769c81f65d1157fcbfab00b92013d70bdda9ff
antoinebekono07-debug <antoinebekono07@gmail.com>
2026-03-18 16:33:01 +0100

    Webpack: disable Sass compilation to avoid node-sass issues; only compile JS

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,8 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
 
 // Ensure sass-loader uses dart-sass implementation to avoid node-sass requirements on newer Node versions
 try {
