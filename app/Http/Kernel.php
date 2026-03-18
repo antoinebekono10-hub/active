6e5275ed79c2241b091c13df68d76989b12595ba
antoinebekono07-debug <antoinebekono07@gmail.com>
2026-03-18 05:20:41 +0100

    Re-add HTTPS headers to Nginx, enable FORCE_HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Language::class,
-            \App\Http\Middleware\HttpsProtocol::class,
+            // \App\Http\Middleware\HttpsProtocol::class,
             \App\Http\Middleware\CheckForMaintenanceMode::class
         ],
 
